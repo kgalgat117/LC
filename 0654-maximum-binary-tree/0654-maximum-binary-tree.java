66ff974ecff02fd9517c769d636abb5d25ca0fd4
Karan Galgat <kgalgat117@gmail.com>
2022-10-15 01:34:54 +0530

    Time: 7 ms (23.53%), Space: 42.1 MB (97.55%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.java
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -15,17 +15,18 @@
  */
 class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-                Deque<TreeNode> stack = new LinkedList<>();
-        for(int n : nums){
-            TreeNode cur = new TreeNode(n);
-            while(!stack.isEmpty() && stack.peek().val < n){
-                cur.left = stack.pop();
+        Deque<TreeNode> stack = new LinkedList<>();
+        for(int i = 0; i < nums.length; i++) {
+            TreeNode curr = new TreeNode(nums[i]);
+            while(!stack.isEmpty() && stack.peek().val < nums[i]) {
+                curr.left = stack.pop();
             }
-            if(!stack.isEmpty()){
-                stack.peek().right = cur;
+            if(!stack.isEmpty()) {
+                stack.peek().right = curr;
             }
-            stack.push(cur);
+            stack.push(curr);
         }
+        
         return stack.isEmpty() ? null : stack.removeLast();
     }
 }
